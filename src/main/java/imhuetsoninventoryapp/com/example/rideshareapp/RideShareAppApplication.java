51ba6b1d0368605f1c05e0f1f693a161d6f68efb
imhuetson <imhuetson@mail.mccneb.edu>
2026-05-14 08:20:09 -0500

    Create Final Application, load in dependencies

---
+++ src/main/java/imhuetsoninventoryapp/com/example/rideshareapp/RideShareAppApplication.java
@@ -0,0 +1,13 @@
+package imhuetsoninventoryapp.com.example.rideshareapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideShareAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideShareAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/imhuetsoninventoryapp/com/example/rideshareapp/RideShareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package imhuetsoninventoryapp.com.example.rideshareapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideShareAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
