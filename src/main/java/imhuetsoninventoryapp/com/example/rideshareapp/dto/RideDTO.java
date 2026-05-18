b0ed4206ac1458ac3f2c72e77f4acb5d0dad79e5
imhuetson <imhuetson@mail.mccneb.edu>
2026-05-18 08:35:19 -0500

    Added rest api dto classes

---
+++ src/main/java/imhuetsoninventoryapp/com/example/rideshareapp/dto/RideDTO.java
@@ -0,0 +1,34 @@
+package imhuetsoninventoryapp.com.example.rideshareapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+public class RideDTO {
+    @Setter
+    @Getter
+    private Long id;
+    @Getter
+    @Setter
+    private String fromLocation;
+    @Getter
+    @Setter
+    private String toCampus;
+    @Getter
+    @Setter
+    private LocalDateTime departureTime;
+    @Getter
+    @Setter
+    private LocalDateTime arrivalTime;
+    @Getter
+    @Setter
+    private int maxPassengers;
+    @Getter
+    @Setter
+    private int currentPassengers;
+    @Getter
+    @Setter
+    private boolean cancelled;
+    public RideDTO() {}
+}
---
+++ src/main/java/imhuetsoninventoryapp/com/example/rideshareapp/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package imhuetsoninventoryapp.com.example.rideshareapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserDTO {
+    @Getter
+    @Setter
+    private Long id;
+    @Getter
+    @Setter
+    private String username;
+    @Getter
+    @Setter
+    private String fullName;
+    @Getter
+    @Setter
+    private String email;
+
+    public UserDTO() {}
+}
