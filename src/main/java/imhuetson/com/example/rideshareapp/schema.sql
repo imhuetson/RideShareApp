37b60eb38b2c80e06247a0ed5ab21201a0897f63
imhuetson <imhuetson@mail.mccneb.edu>
2026-05-18 12:23:46 -0500

    Adding in schema

---
+++ src/main/java/imhuetson/com/example/rideshareapp/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    full_name VARCHAR(150),
+    email VARCHAR(150),
+    role VARCHAR(50),
+);
+
+CREATE TABLE IF NOT EXISTS ride (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    from_location VARCHAR(150) NOT NULL,
+    to_location VARCHAR(150) NOT NULL,
+    departure_time DATETIME,
+    arrival_time DATETIME,
+    max_passengers INT NOT NULL,
+    current_passengers INT DEFAULT 0,
+    status VARCHAR(50),
+    driver_id BIGINT,
+    CONSTRAINT fk_driver FOREIGN KEY (driver_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS ride_passengers (
+    ride_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+
+    PRIMARY KEY (ride_id, user_id),
+
+    CONSTRAINT fk_ride
+        FOREIGN KEY (ride_id)
+        REFERENCES ride(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
